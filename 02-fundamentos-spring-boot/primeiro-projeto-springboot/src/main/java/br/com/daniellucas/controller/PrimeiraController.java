d31b50c86fe230fb257079de6dae4775fe41d3c3
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-05-27 14:59:07 -0300

    path params

--- 02-fundamentos-spring-boot/primeiro-projeto-springboot/src/main/java/br/com/daniellucas/controller/PrimeiraController.java
+++ 02-fundamentos-spring-boot/primeiro-projeto-springboot/src/main/java/br/com/daniellucas/controller/PrimeiraController.java
@@ -1,16 +1,30 @@
 package br.com.daniellucas.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
-  @GetMapping("/primeiroMetodo")
-  public String primeiroMetodo() {
-    return "Mue primiero metodo no spring";
+  @GetMapping("/primeiroMetodo/{id}")
+  public String primeiroMetodo(@PathVariable String id) {
+    return "O parametro e: " + id;
   }
   
+  @GetMapping("/metodoComQueryParams")
+  public String metodoComQueryParams(@RequestParam String id) {
+    return "O parametro com metodoComQueryParams e: " + id;
+  }
+
+  @GetMapping("/metodoComQueryParams2")
+  public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+    return "O parametro com metodoComQueryParams2 e: " + allParams.entrySet();
+  }
 }
