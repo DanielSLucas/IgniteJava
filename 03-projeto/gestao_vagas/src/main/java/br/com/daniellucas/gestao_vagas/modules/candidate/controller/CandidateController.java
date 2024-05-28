7e80f3de3028468ac2be95890d67bb3782f0d27b
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-05-28 11:21:11 -0300

    repositorio de candidato

--- 03-projeto/gestao_vagas/src/main/java/br/com/daniellucas/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ 03-projeto/gestao_vagas/src/main/java/br/com/daniellucas/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.daniellucas.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.daniellucas.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidates")
 public class CandidateController {
   
+  @Autowired
+  private CandidateRepository candidateRepository;
 
   @PostMapping("/")
   public ResponseEntity<CandidateEntity> create(@Valid @RequestBody CandidateEntity candidateEntity) {  
-    return ResponseEntity.status(HttpStatus.CREATED).body(candidateEntity);
+    var entity =this.candidateRepository.save(candidateEntity);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(entity);
   }
   
 }
