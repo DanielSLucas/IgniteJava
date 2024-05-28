e531a17b2538f738d74c3c36656a621a66a2bb22
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-05-28 17:19:13 -0300

    remove unnused imports

--- 03-projeto/gestao_vagas/src/main/java/br/com/daniellucas/gestao_vagas/security/SecurityFilter.java
+++ 03-projeto/gestao_vagas/src/main/java/br/com/daniellucas/gestao_vagas/security/SecurityFilter.java
@@ -1,13 +1,11 @@
 package br.com.daniellucas.gestao_vagas.security;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
