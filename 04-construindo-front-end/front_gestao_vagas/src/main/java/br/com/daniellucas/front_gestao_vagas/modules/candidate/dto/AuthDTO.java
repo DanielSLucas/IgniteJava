e7314dca98a26dd97bcf1d7d17560a8a9b748047
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-08-08 16:41:12 -0300

    login chamando a /auth

---
+++ 04-construindo-front-end/front_gestao_vagas/src/main/java/br/com/daniellucas/front_gestao_vagas/modules/candidate/dto/AuthDTO.java
@@ -0,0 +1,8 @@
+package br.com.daniellucas.front_gestao_vagas.modules.candidate.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDTO {
+  private String access_token;
+}
---
+++ 04-construindo-front-end/front_gestao_vagas/src/main/java/br/com/daniellucas/front_gestao_vagas/modules/candidate/service/CandidateService.java
@@ -0,0 +1,36 @@
+package br.com.daniellucas.front_gestao_vagas.modules.candidate.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.daniellucas.front_gestao_vagas.modules.candidate.dto.AuthDTO;
+
+@Service
+public class CandidateService {
+  public AuthDTO login(String username, String password) {
+    RestTemplate rt = new RestTemplate();
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    Map<String, String> data = new HashMap<>();
+    data.put("username", username);
+    data.put("password", password);
+
+    HttpEntity<Map<String, String>> request = new HttpEntity<>(data);
+
+    var result = rt.postForObject(
+      "http://localhost:8080/candidate/auth", 
+      request, 
+      AuthDTO.class
+    );
+
+    return result;
+  }
+}
