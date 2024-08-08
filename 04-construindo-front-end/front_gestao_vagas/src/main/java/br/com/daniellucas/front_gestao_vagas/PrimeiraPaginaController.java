91ea0714febce2e13f6af7864c976bb40094173c
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-08-08 14:25:57 -0300

    primeira pagina com thymeleaf

---
+++ 04-construindo-front-end/front_gestao_vagas/src/main/java/br/com/daniellucas/front_gestao_vagas/PrimeiraPaginaController.java
@@ -0,0 +1,14 @@
+package br.com.daniellucas.front_gestao_vagas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PrimeiraPaginaController {
+  @GetMapping("/home")
+  public String primeiraPaginaHtml() {
+    return "primeiraPagina";
+  }
+}
